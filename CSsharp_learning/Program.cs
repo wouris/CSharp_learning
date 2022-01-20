4138c92cc388f812823b105ca2a57afd5aebc09a
wouris <wouris44@gmail.com>
2022-01-20 20:34:02 +0100

    Added a menu with 1 function

--- CSsharp_learning/Program.cs
+++ CSsharp_learning/Program.cs
@@ -8,6 +8,32 @@ namespace CSsharp_learning
     {
         static void Main(string[] args)
         {
+
+            ConsoleKeyInfo input;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Select a program you want to run:");
+                Console.WriteLine();    //insert [enter]
+                Console.WriteLine("1. Slicing a word or a sentence");
+                Console.WriteLine();    //insert [enter]
+                Console.WriteLine("Press 'q' to exit.");
+                Console.Write(">> ");
+                input = Console.ReadKey();
+                    
+                if (input.Key == ConsoleKey.D1)
+                {
+                    SlicingWord();                    
+                }
+            } while (input.Key != ConsoleKey.Q);
+
+            Console.Write("\nGoodbye!");
+
+        }
+
+        static void SlicingWord()
+        {
+            Console.Clear();
             Console.WriteLine("Type any sentence or a word:");
             var uInput = Console.ReadLine();
             var typeOfUInput = "word";
@@ -15,7 +41,7 @@ namespace CSsharp_learning
             {
                 typeOfUInput = "sentence";
             }
-            Console.WriteLine($"The {typeOfUInput} \"{uInput}\" contains {uInput.Length} letters in it!");
+            Console.WriteLine($"The {typeOfUInput} \"{uInput}\" consists of {uInput.Length} letters!");
 
             int count = 0;
             while (true)
@@ -55,8 +81,10 @@ namespace CSsharp_learning
                 Console.WriteLine();
             }
 
-            EndOfApplication: ;
 
+            EndOfApplication: 
+            System.Console.ReadKey();
+            Console.Clear();
         }
     }
 }
